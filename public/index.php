977224e204ace275906508d66568d08400389dd7
Евгений Г.М <evgeniy.goryachkin@rt.ru>
2022-03-16 18:04:13 +0300

    Done work

--- public/index.php
+++ public/index.php
@@ -7,12 +7,12 @@ $dom = phpQuery::newDocument($html);
 
 $articles = array();
 $i = 0;
+
+// Входим на главную страницу
 foreach($dom->find('.article-card__small-wrapper') as $key => $value){
     if ($i > 15) break;
     $pq = pq($value);
     $articles[$key]['href'] = str_replace('https://lifehacker.ru/', '', $pq->find('.lh-small-article-card__link')->attr('href'));
-    // $articles[$key]['href'] = str_replace('https://lifehacker.ru/', '', $articles[$key]['href']);
-    // $articles[$key]['href'] = trim('https://lifehacker.ru/');
     $articles[$key]['article-name'] = $pq->find('.lh-small-article-card__link')->attr('name');
     $articles[$key]['image'] = $pq->find('.lh-small-article-card__cover')->attr('src');
     $i++;
@@ -20,42 +20,19 @@ foreach($dom->find('.article-card__small-wrapper') as $key => $value){
 
 phpQuery::unloadDocuments();
 
-// $articlesText = array();
-
-// $currenArticles = file_get_contents('https://lifehacker.ru/tovary-dlya-remonta-gadzhetov/');
-// $domCurrentArticle = phpQuery::newDocument($currenArticles);
-// $articlesArr = array();
-// $articlesPage = $domCurrentArticle->find('.single-article__post-content p');
-//     foreach($articlesPage as $text){
-//         $pq = pq($text);
-//         $articlesArr .= $pq->text();
-//         // var_dump($pq->text());
-//         // echo $pq->text();
-//     }
-// // echo $domCurrentArticle;
-// // var_dump($articlesArr);
+// Парсим статьи
 foreach($articles as $key => $value) {
     $currenArticles = file_get_contents('https://lifehacker.ru' . $articles[$key]['href']);
     $domCurrentArticle = phpQuery::newDocument($currenArticles);
     $articlesPage = $domCurrentArticle->find('.single-article__post-content p');
     $textArticle = '';
-    // var_dump($articlesPage);
-    // $articles[$key]['text'] = $pq->find('.single-article__post-content')->find('p')->text();
-    // foreach($domCurrentArticle->find('.single-article__post-content') as $key => $value) {
-    //     $pq = pq($value);
-    //     $articlesText[$key]['text'] = $pq->find('p')->text();
-    // }
     foreach($articlesPage as $text){
         $pq = pq($text);
         $textArticle .= $pq->text();
-        // var_dump($pq->text());
-        // echo $pq->text();
     }
     $articles[$key]['text'] = $textArticle;
     phpQuery::unloadDocuments();
 }
-
-// var_dump($articles);
 ?>
  <html>
      <head>
@@ -85,8 +62,16 @@ foreach($articles as $key => $value) {
                                     <?php 
                                         $str = substr($articles[$key]['text'], 0, 200);
                                         echo substr($str, 0, strrpos($str, ' ' )) . '...'; 
-                                        ?>
+                                    ?>
                                 </p>
+                                <a  class="btn btn-primary" data-bs-toggle="collapse" href="#collapse-<?= $key ?>" role="button" aria-expanded="false" aria-controls="collapse-<?= $key ?>">
+                                    Читать
+                                </a>
+                                <div class="collapse" id="collapse-<?= $key ?>">
+                                    <div>
+                                       <?= $articles[$key]['text'] ?>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
