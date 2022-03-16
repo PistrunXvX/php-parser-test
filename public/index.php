50b189b05ef05b0d08264c30b91d76f605ec9b7d
Евгений Г.М <evgeniy.goryachkin@rt.ru>
2022-03-16 17:35:25 +0300

    frontend part 1/2 done

--- public/index.php
+++ public/index.php
@@ -55,7 +55,46 @@ foreach($articles as $key => $value) {
     phpQuery::unloadDocuments();
 }
 
-var_dump($articles);
-
-
+// var_dump($articles);
 ?>
+ <html>
+     <head>
+         <title>
+             PHP Parser
+         </title>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     </head>
+     <body>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>Новости</h1>
+                    </div>
+                </div>
+                <?php foreach($articles as $key => $value): ?>
+                <div class="row" style="margin-top: 10%;">
+                    <div class="col-6">
+                        <div class="card">
+                            <img src="<?= $articles[$key]['image']; ?>" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <?= $articles[$key]['article-name']; ?>
+                                </h5>
+                                <p class="card-text">
+                                    <?php 
+                                        $str = substr($articles[$key]['text'], 0, 200);
+                                        echo substr($str, 0, strrpos($str, ' ' )) . '...'; 
+                                        ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+     </body>
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+ </html>
